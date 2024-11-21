137cd6abcf5069c032a25a6b945207c798550426
MaciekZ23 <maciekz232002@o2.pl>
2024-11-21 08:58:45 +0100

    Funkcja

---
+++ functions.js
@@ -0,0 +1,13 @@
+function isOdd(x)
+{
+    return x % 2 != 0;
+}
+
+const isEven = (x) => x % 2 == 0;
+
+const x = 4;
+console.log(isOdd(x));
+console.log(isEven(x));
+const y = 5;
+console.log(isOdd(y));
+console.log(isEven(y))
